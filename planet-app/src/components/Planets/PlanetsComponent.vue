78f6d19c4d8245985b0eb71da233568d355a8eb5
tbunitrade <tbunitrade@gmail.com>
2024-07-26 20:12:34 +0300

    create the sun

--- planet-app/src/components/Planets/PlanetsComponent.vue
+++ planet-app/src/components/Planets/PlanetsComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <Planet name="Нейминг адрес сайта" :orbitSize="300" :initialAngle="1000" :orbitSpeed="17" v-if="!isExpanding" />
-    <Planet name="Прототипирование проекта" :orbitSize="500" :initialAngle="1000" :orbitSpeed="15" v-if="!isExpanding" />
-    <Planet name="Дизайн для Веба" :orbitSize="550" :initialAngle="1000" :orbitSpeed="20" v-if="!isExpanding" />
-    <Planet name="Вертска дизайна" :orbitSize="500" :initialAngle="1000" :orbitSpeed="25" v-if="!isExpanding" />
-    <Planet name="Обратная связь" :orbitSize="600" :initialAngle="1000" :orbitSpeed="30" v-if="!isExpanding" />
+    <Planet name="Нейминг адрес сайта" :orbitSize="300" :initialAngle="10" :orbitSpeed="17" v-if="!isExpanding" />
+    <Planet name="Прототипирование проекта" :orbitSize="500" :initialAngle="250" :orbitSpeed="15" v-if="!isExpanding" />
+    <Planet name="Дизайн для Веба" :orbitSize="550" :initialAngle="500" :orbitSpeed="20" v-if="!isExpanding" />
+    <Planet name="Вертска дизайна" :orbitSize="500" :initialAngle="700" :orbitSpeed="25" v-if="!isExpanding" />
+    <Planet name="Обратная связь" :orbitSize="600" :initialAngle="700" :orbitSpeed="30" v-if="!isExpanding" />
   </div>
 </template>
 
