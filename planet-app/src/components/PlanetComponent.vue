a0c6021a2f21a24f082d781b387f564f39a05588
tbunitrade <tbunitrade@gmail.com>
2024-07-26 19:17:08 +0300

    create orbit for our planet

--- planet-app/src/components/PlanetComponent.vue
+++ planet-app/src/components/PlanetComponent.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="planet" @mouseover="isHovered = true" @mouseleave="isHovered = false">
-    <div v-if="isHovered" class="tooltip">{{ name }}</div>
+  <div class="orbit">
+    <div class="planet" @mouseover="isHovered = true" @mouseleave="isHovered = false">
+      <div v-if="isHovered" class="tooltip">{{ name }}</div>
+    </div>
   </div>
 </template>
 
@@ -22,17 +24,32 @@ export default {
 </script>
 
 <style scoped>
+.orbit {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 200px;
+  height: 200px;
+  margin: -100px 0 0 -100px;
+  animation: orbit 10s linear infinite;
+}
+
 .planet {
   width: 100px;
   height: 100px;
   background-color: gray;
   border-radius: 50%;
-  position: relative;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
   transition: transform 0.3s ease;
 }
+
 .planet:hover {
-  transform: scale(1.2);
+  transform: translateX(-50%) scale(1.2);
 }
+
 .tooltip {
   position: absolute;
   top: -20px;
@@ -42,5 +59,14 @@ export default {
   color: white;
   padding: 5px;
   border-radius: 5px;
+}
+
+@keyframes orbit {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
